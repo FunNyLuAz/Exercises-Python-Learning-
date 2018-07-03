c920a6b28c98fc0240c32a92ef454d737b6407ea
FunNyLuAz <ladsword@gmail.com>
2018-07-03 17:57:07 -0300

    Some Updates

--- exer076.py
+++ exer076.py
@@ -1,12 +1,12 @@
 lp = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor',
       4.2, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.9)
 
-print('{:=^50}'.format(' < Lista de Produtos > '))
+print(f'{" < Lista de Produtos > ":=^50}')
 
 for ct, c in enumerate(lp):
     if ct % 2 == 0:
-        print('> {:.<38}'.format(c), end='')
+        print(f'> {c:.<38}', end='')
     else:
-        print('R$:{:>7.2f}'.format(lp[ct]))
+        print(f'R$:{lp[ct]:>7.2f}')
 
 print('='*50)
