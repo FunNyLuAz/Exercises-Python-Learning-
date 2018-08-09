e47dae7bb81676b90797bc3eaaf765ff72843d28
FunNyLuAz <ladsword@gmail.com>
2018-08-09 20:04:14 -0300

    Updated

--- exer088.py
+++ exer088.py
@@ -5,7 +5,7 @@ print(f'{"< Gerador de Palpites >":-^31}')
 
 q = int(input('Quantidade de palpites: '))
 p = []
-c = ct = nr = 0
+c = ct = 0
 
 print('PROCESSANDO...\n')
 sleep(0.7)
@@ -15,11 +15,38 @@ while not c == q:
         nr = randint(1, 60)
         if nr not in p:
             p.append(nr)
-        else:
-            ct -= 1
-        ct += 1
+            ct += 1
     p.sort()
     print(f'{c+1}º Palpite: {p}')
     p.clear()
     ct = 0
     c += 1
+
+#Outra Maneira:
+'''from random import randint
+from time import sleep
+
+print(f'{"< Gerador de Palpites >":-^31}')
+
+q = int(input('Quantidade de palpites: '))
+tp = []
+p = []
+c = ct = 0
+
+while c < q:
+    while not ct == 6:
+        nr = randint(1, 60)
+        if nr not in p:
+            p.append(nr)
+            ct += 1
+    p.sort()
+    tp.append(p[:])
+    p.clear()
+    ct = 0
+    c += 1
+
+for p, l in enumerate(tp):
+    print(f'{p+1}º Palpite: {l}')
+    sleep(1)
+
+print('\nBoa sorte!')'''
